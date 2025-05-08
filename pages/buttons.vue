e9c4fd65d9283dc08aaddae822bbd614b63f6c0c
hoffmannagnes <hoffmannagi80@gmail.com>
2025-05-08 10:56:14 +0200

    init

---
+++ pages/buttons.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+      <h1>Gombos oldal</h1>
+      <button @click="firstAction">Első gomb</button>
+      <button @click="secondAction">Második gomb</button>
+    </div>
+  </template>
+  
+  <script setup>
+  const firstAction = () => {
+    alert('Első gomb megnyomva');
+  }
+  
+  const secondAction = () => {
+    alert('Második gomb megnyomva');
+  }
+  </script>
+  
+  <style scoped>
+  .container {
+    padding: 2rem;
+    text-align: center;
+  }
+  
+  button {
+    margin: 1rem;
+    padding: 0.75rem 1.5rem;
+    font-size: 1rem;
+  }
+  </style>
+  
