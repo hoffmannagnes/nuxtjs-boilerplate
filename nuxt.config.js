2327257893dd19fc1fcbcdce257b1059c52148d9
hoffmannagnes <hoffmannagi80@gmail.com>
2025-05-09 11:50:56 +0200

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,20 +5,24 @@ export default {
     // prerender index route by default
     '/': { prerender: true },
   },
-  app: {
-    cdnURL: '/',
-    head: {
-      script: [
-        {
-          hid: 'gtmHead',
-          innerHTML: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-*');`,
-        },
-      ],
-    },
-  },
+  head: {
+    script: [
+      {
+        hid: 'gtag',
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-E39NSFHYTT'
+      },
+      {
+        hid: 'gtag-config',
+        innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-E39NSFHYTT'); // Az Analytics mérőkódod
+        `,
+        type: 'text/javascript'
+      }
+    ],
+    __dangerouslyDisableSanitizers: ['script']
+  }
 };
