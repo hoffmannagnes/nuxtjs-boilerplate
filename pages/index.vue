e8395f7a33c89537a32899fdfcf520eabc3e4a4b
hoffmannagnes <hoffmannagi80@gmail.com>
2025-05-09 10:27:32 +0200

    tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
     <div class="container">
       <h1>Gombos oldal</h1>
-      <button @click="firstAction">Első gomb</button>
-      <button @click="secondAction">Második gomb</button>
+      <button @click="firstAction" data-tag="login">Első gomb</button>
+      <button @click="secondAction" data-tag="registration">Második gomb</button>
     </div>
   </template>
   
   <script setup>
-  const firstAction = () => {
-    alert('Első gomb megnyomva');
-  }
-  
-  const secondAction = () => {
-    alert('Második gomb megnyomva');
-  }
+const firstAction = (event) => {
+    const tag = event.target.getAttribute("data-tag");
+    sendEventToAnalytics(tag);
+};
+
+const secondAction = (event) => {
+    const tag = event.target.getAttribute("data-tag");
+    sendEventToAnalytics(tag);
+};
+
+// Esemény küldése analitikához
+const sendEventToAnalytics = (tag) => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "button_click",
+        button_type: tag // Login vagy Registration
+    });
+
+    console.log(`Event sent: ${tag}`);
+};
   </script>
   
   <style scoped>
